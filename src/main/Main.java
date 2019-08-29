30c5ab7ff96ecc40d1482cb785f18f0c6e76ce72
Splerge <damnsplurge@gmail.com>
2019-08-29 22:13:01 +1000

    changed rom load to be an argument

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,11 +3,13 @@ package main;
 import cpu.GameBoyCPU;
 import mmu.GameBoyMMU;
 
+import java.io.File;
+
 public class Main {
 	public static void main(String[] args) {
 		try {
 			GameBoyMMU memory = GameBoyMMU.getInstance();
-			memory.initialize("DMG_ROM.bin");
+			memory.initialize(args[0]);
 			System.out.println(Utility.encodeHexString(memory.getMemory()));
 			GameBoyCPU cpu = GameBoyCPU.getInstance();
 			cpu.run();
